1477640d2092eaaa0c67f994da36a48ab8726f20
Samantha Leong <samileong@berkeley.edu>
2025-11-07 01:02:14 +0000

    completed small tests for readligo.py

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,21 +1,21 @@
-import os
 import numpy as np
 from ligotools import readligo
 
 TEST_FILE = "data/H-H1_LOSC_4_V2-1126259446-32.hdf5"
 
-def test_loaddata_output_structure():
-    """Check that loaddata returns strain, time, and metadata"""
-    strain, time, meta = readligo.loaddata(TEST_FILE)
+def test_loaddata_returns_arrays_and_dict():
+    """Check that loaddata returns strain and time arrays, and a channel dictionary"""
+    strain, time, channel_dict = readligo.loaddata(TEST_FILE)
     
     assert isinstance(strain, np.ndarray), "Strain should be a numpy array"
     assert isinstance(time, np.ndarray), "Time should be a numpy array"
-    assert isinstance(meta, dict), "Metadata should be a dictionary"
+    assert isinstance(channel_dict, dict), "Channel dict should be a dictionary"
     
-    assert len(strain) == len(time), "Strain and time arrays should have the same length"
+    assert len(strain) == len(time), "Strain and time arrays should have same length"
 
-def test_metadata_contains_expected_keys():
-    """Check that metadata has standard keys from LOSC files"""
-    _, _, meta = readligo.loaddata(TEST_FILE)
-    for key in ["Detector", "UTCstart", "Duration"]:
-        assert key in meta, f"Metadata should contain '{key}' key"
+def test_channel_dict_contains_default():
+    """Check that channel dictionary contains DEFAULT key"""
+    _, _, channel_dict = readligo.loaddata(TEST_FILE)
+    
+    assert 'DEFAULT' in channel_dict, "Channel dictionary should contain 'DEFAULT' key"
+    assert isinstance(channel_dict['DEFAULT'], np.ndarray), "'DEFAULT' channel should be an array"
